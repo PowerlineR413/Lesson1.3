1a235c82db77aa9342269a7ad57cc45260081640
PowerlineR413 <Powerline413@yandex.ru>
2023-02-16 19:34:17 +0500

    Урок 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author El Baron 14.02.2023
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("*****************************\n");
+        System.out.print("****\tВизитка\t\t\t*****\n");
+        System.out.print("**** Name: \"Dmitriy\" \t*****\n");
+        System.out.println("**** Skype: paladinre413 ****");
+        System.out.println("*****************************");
+
+    }
+}
